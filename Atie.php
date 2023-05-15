912d339e397e6e875bfb86f9922b4681aa3e297a
AtiehRabani <atierabani@gmail.com>
2023-05-15 19:14:54 +0430

    first project

---
+++ Atie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+       <title> Project1 </title>
+
+    </head>
+
+        <body>
+            
+            <?php
+            $name="Atie";
+            $lastname = "Rabani";
+             ?>
+             <h1>WELCOME!</h1>
+             <h3> my name is <?php echo $name?></h3>
+             <h3> my last name is <?php echo $lastname?>
+
+            
+            
+        </body>
+    
+</HTML> 
