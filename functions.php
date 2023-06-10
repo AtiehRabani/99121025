09a4ed13dab887e7c6cddc5b8b8ecbd79f78af6c
AtiehRabani <126250488+AtiehRabani@users.noreply.github.com>
2023-06-11 01:57:23 +0330

    finall lesson

--- functions.php
+++ functions.php
@@ -5,3 +5,21 @@ function dd($in)
     var_dump($in);
     die;
 }
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI']==$value;
+}
+function abort($code='404')
+{
+    http_response_code($code);
+    require("views/{$code}.view.php");
+    die();
+}
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if(!$condition)
+    {
+        abort($status);
+    }
+    return true;
+}
